009c52e1cf7d9c25262eb099563c3880330149e1
Sebastian Fernandez <sebastianfernandez@MacBook-Air-de-sebastian.local>
2023-02-08 18:01:52 -0300

    primer commit

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import { Button } from '@chakra-ui/react'
 
 const CartWidget = () => {
   return (
-    <div>
+    <div className='cart'>
 <Button size="lg" variant="outline" colorScheme='blue'>
     <span className='material-symbols-outlined'> shopping_cart</span>
     <span> 7 </span>
